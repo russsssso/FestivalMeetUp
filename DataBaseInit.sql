320bcdb96d92794d3e93623bb491a53d33a781ce
russsssso <rusuandrei6060@gmail.com>
2024-04-16 11:54:01 +0300

    DB small adjustments;DB Population;

--- DataBaseInit.sql
+++ DataBaseInit.sql
@@ -15,7 +15,8 @@ DROP TABLE Users;
 
 CREATE TABLE Users
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
+	Display_Name	VARCHAR(50),
 	Facebook		VARCHAR(300),
 	Instagram		VARCHAR(300),
 );
@@ -23,13 +24,13 @@ CREATE TABLE Users
 CREATE TABLE Accounts
 (
 	Username		VARCHAR(100) NOT NULL PRIMARY KEY,
-	Password_Hash	VARCHAR(100) NOT NULL, 
+	Password_Hash	VARBINARY(300) NOT NULL, 
 	User_ID			BIGINT NOT NULL FOREIGN KEY REFERENCES Users(ID)
 );
 
 CREATE TABLE Addresses
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
 	Country			VARCHAR(50),
 	County			VARCHAR(50),
 	City			VARCHAR(50),
@@ -40,7 +41,8 @@ CREATE TABLE Addresses
 
 CREATE TABLE Events
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
+	Name			VARCHAR(100) NOT NULL,
 	Creator_User	BIGINT NOT NULL FOREIGN KEY REFERENCES Users(ID),
 	Event_Address	BIGINT NOT NULL FOREIGN KEY REFERENCES Addresses(ID),
 	Starting_Date	DATETIME NOT NULL,
@@ -55,13 +57,13 @@ CREATE TABLE Participating_Users
 
 CREATE TABLE Event_Report_Types
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
 	Name			VARCHAR(50)
 );
 
 CREATE TABLE Event_Reports
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
 	Submitter		BIGINT NOT NULL FOREIGN KEY REFERENCES Users(ID),
 	Target_Event	BIGINT NOT NULL FOREIGN KEY REFERENCES Events(ID),
 	Report_Type		BIGINT NOT NULL FOREIGN KEY REFERENCES Event_Report_Types(ID),
@@ -70,13 +72,13 @@ CREATE TABLE Event_Reports
 
 CREATE TABLE User_Report_Types
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
 	Name			VARCHAR(50)
 );
 
 CREATE TABLE User_Reports
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
 	Submitter		BIGINT NOT NULL FOREIGN KEY REFERENCES Users(ID),
 	Target_User		BIGINT NOT NULL FOREIGN KEY REFERENCES Users(ID),
 	Report_Type		BIGINT NOT NULL FOREIGN KEY REFERENCES User_Report_Types(ID),
@@ -85,13 +87,54 @@ CREATE TABLE User_Reports
 
 CREATE TABLE Feeds
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
 	Event_ID		BIGINT NOT NULL FOREIGN KEY REFERENCES Events(ID),
 );
 
 CREATE TABLE Posts
 (
-	ID				BIGINT NOT NULL PRIMARY KEY,
+	ID				BIGINT NOT NULL IDENTITY PRIMARY KEY,
 	Feed_ID			BIGINT NOT NULL FOREIGN KEY REFERENCES Feeds(ID),
+	User_ID			BIGINT NOT NULL FOREIGN KEY REFERENCES Users(ID),
 	Post_Text		VARCHAR(500)
 );
+
+INSERT INTO Users(Display_Name, Facebook, Instagram)
+	VALUES	('admin', NULL, NULL),
+			('russsssso', NULL, NULL),
+			('antonia', NULL, NULL),
+			('predoi', NULL, NULL);
+
+SELECT * FROM Users;
+
+INSERT INTO Accounts(Username, Password_Hash, User_ID)
+	VALUES	('admin',	HASHBYTES('SHA2_256', CONVERT(NVARCHAR(30), 'admin')),	1),
+			('ruso',	HASHBYTES('SHA2_256', CONVERT(NVARCHAR(30), 'parola')), 2),
+			('antonia',	HASHBYTES('SHA2_256', CONVERT(NVARCHAR(30), 'parola')), 3),
+			('predoi',	HASHBYTES('SHA2_256', CONVERT(NVARCHAR(30), 'parola')), 4);
+
+SELECT * FROM Accounts;
+
+INSERT INTO Addresses(Country, County, City, Street, Number, ZIP)
+	VALUES	('Romania', 'Bucuresti', 'Sector 6', 'Splaiul Independentei', 290, 600409),
+			('Romania', 'Cluj', 'Cluj', 'Strada din Cluj', 1, 888888);
+
+SELECT * FROM Addresses;
+
+INSERT INTO Events(Name, Creator_User, Event_Address, Starting_Date, Ending_Date)
+	VALUES	('Untold', 1, 2, '20240718 8:00:00 AM', '20240720 10:00:00 PM'),
+			('Cafea in Regie', 2, 1, '20240701 8:00:00 AM', '20240701 10:00:00 AM');
+
+INSERT INTO Participating_Users(User_ID, Event_ID)
+	VALUES	(2, 1), (3, 1), (4, 1),
+			(2, 2);
+
+SELECT * FROM Participating_Users;
+
+INSERT INTO Feeds(Event_ID)
+	VALUES (1), (2);
+
+INSERT INTO Posts(Feed_ID, Post_Text, User_ID)
+	VALUES (2, 'Post entuziast despre cafea!', 2);
+
+SELECT * FROM Posts;
